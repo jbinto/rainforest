d26ac4b4883427924e6c3af5215307d5031be5f9
Matt Vleming <mattvleming@gmail.com>
2013-03-25 17:15:53 -0400

    Add error messages to signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,4 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   validates :name, :presence => true
   validates :email, { :presence => true, :uniqueness => true }
-
 end
