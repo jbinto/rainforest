5c0dbc46de4354c2b95548333869b9d0bbeb5c8c
Jesse Buchanan <jbinto@jbinto.ca>
2013-03-25 09:22:59 -0400

    Set factory_girl as fixtures replacement

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ end
 
 module Rainforest
   class Application < Rails::Application
+
+    # So we can run "rails generate" from the command line to create factories
+    config.generators do |g|
+      g.fixture_replacement :factory_girl
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
