c423deba5320ec572cd26a8254b6b6ccae9a7f9e
Matt Vleming <mattvleming@gmail.com>
2013-03-25 11:28:31 -0400

    Add validation (and tests\!) for price_in_dollars

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ActiveRecord::Base
 
   validates :description, :name, :presence => true
   validates :price_in_cents, :numericality => {:only_integer => true}
+  validates :price_in_dollars, :numericality => { greater_than: 0 }
 
   def price_in_dollars
   	self.price_in_cents.to_f / 100.0
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -30,4 +30,25 @@ class ProductTest < ActiveSupport::TestCase
     @p.price_in_cents = 4.50
     refute @p.valid?
   end
+
+  test "must have a numerical price_in_dollars" do
+    @p.price_in_dollars = "bar"
+    refute @p.valid?
+  end
+
+  test "must have a greater-than-zero price_in_dollars" do
+    @p.price_in_dollars = "-3403"
+    refute @p.valid?
+  end
+
+  test "price_in_dollars accepts decimal and converts to cents correctly" do
+    @p.price_in_dollars = "123.45"
+    assert @p.price_in_cents == 12345
+  end
+
+  test "price_in_dollars displays an existing price_in_cents correctly" do
+    @p.price_in_cents = 12345
+    assert @p.price_in_dollars == 123.45
+  end
+
 end
