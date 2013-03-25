5b56069684aa2cbaf42a332efc0293952f8804bf
Matt Vleming <mattvleming@gmail.com>
2013-03-25 10:15:24 -0400

    Add validations for price, name, description

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @p = FactoryGirl.create(:product)
+    assert @p.valid?
+  end
+
+  test "must have a description" do
+    @p.description = ""
+    refute @p.valid?
+  end
+
+  test "must have a name" do
+    @p.name = ""
+    refute @p.valid?
+  end
+
+  test "must have a numerical price_in_cents" do
+    @p.price_in_cents = "foo"
+    refute @p.valid?
+  end
 end
